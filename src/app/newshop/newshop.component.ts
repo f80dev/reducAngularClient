7b21767447414f1cc60833462757fb191101eab0
f80dev <rv@f80.fr>
2019-10-06 12:21:49 +0200

    passage sur reducshare.com

--- src/app/newshop/newshop.component.ts
+++ src/app/newshop/newshop.component.ts
@@ -53,6 +53,38 @@ export class NewshopComponent implements OnInit {
     this.router.navigate(['home'],{queryParams:{message:"Enseigne non créée"}});
   }
 
+  refresh_map(){
+    clearTimeout(this.handle);
+    this.handle=setTimeout(()=>{
+      debugger
+      var l=getMarkerLayer(this.map);
+      //var features=l.getSource().getFeatures();
+      var center_pos=ol.proj.toLonLat(this.map.getView().getCenter());
+      this.lng=center_pos[0];
+      this.lat=center_pos[1];
+      if(this.reverseGeocode){
+        this.loc.getAddressFromCoord(this.lat,this.lng,(res)=>{
+          this.reverseGeocode=false;
+          if(res.display_name){
+            this.shopname="";
+            "kindergarten,building,pub,hairdresser,supermarket,bar".split(",").forEach((type)=>{
+              if(res.address[type]!=null)this.shopname=res.address[type];
+            });
+            var house_number=res.address.house_number;
+            if(house_number==null && this.shopname!=null)house_number=this.shopname;
+
+            if(res.address.road==null)
+              this.address=res.display_name;
+            else
+              this.address=(normeString(house_number)+", "+res.address.road+", "+res.address.postcode+" "+res.address.city).trim();
+          }
+        },null);
+      }
+      l.getSource().clear();
+      l.getSource().addFeature(createMarker(center_pos[0], center_pos[1],this.config.values.icon_shop,null,0.2));
+    },500);
+  }
+
 
   showOnMap() {
     this.loc.getAddress(this.address,(res)=> {
@@ -62,34 +94,7 @@ export class NewshopComponent implements OnInit {
       if(this.map==null){
         this.map =createMap({lng:this.lng,lat:this.lat},this.config.values.icon_shop,18,0.2,(event)=>{
           //Déplacement de la carte
-          clearTimeout(this.handle);
-          this.handle=setTimeout(()=>{
-            var l=getMarkerLayer(this.map);
-            //var features=l.getSource().getFeatures();
-            var center_pos=ol.proj.toLonLat(this.map.getView().getCenter());
-            this.lng=center_pos[0];
-            this.lat=center_pos[1];
-            if(this.reverseGeocode){
-              this.loc.getAddressFromCoord(this.lat,this.lng,(res)=>{
-                this.reverseGeocode=false;
-                if(res.display_name){
-                  this.shopname="";
-                  "kindergarten,building,pub,hairdresser,supermarket,bar".split(",").forEach((type)=>{
-                    if(res.address[type]!=null)this.shopname=res.address[type];
-                  });
-                  var house_number=res.address.house_number;
-                  if(house_number==null && this.shopname!=null)house_number=this.shopname;
-
-                  if(res.address.road==null)
-                    this.address=res.display_name;
-                  else
-                    this.address=(normeString(house_number)+", "+res.address.road+", "+res.address.postcode+" "+res.address.city).trim();
-                }
-              },null);
-            }
-            l.getSource().clear();
-            l.getSource().addFeature(createMarker(center_pos[0], center_pos[1],this.config.values.icon_shop,null,0.2));
-          },500);
+          this.refresh_map();
         });
       }
       else{
@@ -101,4 +106,13 @@ export class NewshopComponent implements OnInit {
   }
 
 
+  localize() {
+    this.loc.getPosition().then((pos:any)=>{
+      this.lng=pos.lng;
+      this.lat=pos.lat;
+      this.reverseGeocode=true;
+      this.map.getView().setCenter(ol.proj.fromLonLat([this.lng, this.lat]));
+      this.refresh_map();
+    });
+  }
 }
