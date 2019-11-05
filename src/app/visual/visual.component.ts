c241181f22e9706ba1549da5cb763fc869ff008e
f80dev <rv@f80.fr>
2019-11-05 11:29:45 +0100

    Ajout de l'image selector

--- src/app/visual/visual.component.ts
+++ src/app/visual/visual.component.ts
@@ -8,20 +8,17 @@ import {Component, Input, OnInit} from '@angular/core';
 export class VisualComponent implements OnInit {
 
   @Input("picture") picture="";
-  @Input("size") size:string="150px";
   @Input("width") w:string="";
   @Input("height") h:string="";
+
+  @Input("max-width") mw:string="";
+  @Input("max-height") mh:string="";
+
 
   constructor() { }
 
   ngOnInit() {
-    if(!this.size.endsWith("px"))this.size=this.size+"px";
-    if(this.w==null)this.w=this.size;
-    if(this.h==null)this.h=this.size;
-    this.w=""+this.w;
-    this.h=""+this.h;
-    if(!this.w.endsWith("px"))this.w=this.w+"px";
-    if(!this.h.endsWith("px"))this.h=this.h+"px";
+
   }
 
 }
