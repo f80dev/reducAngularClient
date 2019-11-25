82b53eadebc8670310f4b1349347108a5e9a99c7
f80dev <rv@f80.fr>
2019-11-25 11:34:55 +0100

    Possibilité d'éditer les points de ventes

--- src/app/new-coupon-simple/new-coupon-simple.component.ts
+++ src/app/new-coupon-simple/new-coupon-simple.component.ts
@@ -102,7 +102,7 @@ export class NewCouponSimpleComponent implements OnInit {
     this.coupon=compute(this.coupon);
     var color="#400064";
     this.master_text=exportToHTML("L'intro de votre promotion, la phrase choc qui donne envie de la récupérer:" +
-      "<br> #label=teaser_de_votre_promotion <br><br>Le @beneficiaire gagne des #unity=unité_désignant_ce_que_gagne_le_client représenté(e) " +
+      "<br> #label=teaser_de_votre_promotion <br><br>Le @beneficiaire gagne des #unity=unité_désignant_ce_que_gagne_le_client représenté(e)s " +
       "dans REDUCSHARE par le symbole #symbol=Symbole_de_l_unité",this.coupon,(fields)=>{this.add_event(fields);},color);
 
     this.condition_text=exportToHTML("<br><br> Cette offre est valable #conditions=Conditions_pour_bénéficier_de_la_promotion <br>",this.coupon,(fields)=>{this.add_event(fields);},color);
