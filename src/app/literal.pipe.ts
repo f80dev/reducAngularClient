309fc9e1907c79be4aa99f98827974b76bad4998
f80dev <rv@f80.fr>
2019-12-20 10:38:33 +0100

    Possibilité d'éditer les points de ventes

--- src/app/literal.pipe.ts
+++ src/app/literal.pipe.ts
@@ -8,16 +8,19 @@ export class LiteralPipe implements PipeTransform {
   transform(value: number,decimal:number=2,lang: string="fr"): string {
     let rc="";
 
-    for(let k=2;k<=10;k++){
-      if(value==1/k)rc="1 "+k+"ème";
-    }
 
-    if(Math.trunc(value*100)==33)rc="1/3";
 
-    if(rc.length==0 && value<1) {
-      if (value * 10 == Math.trunc(value * 10))
-        rc = value * 100 + "% d'1";
-    }
+    //for(let k=2;k<=10;k++)if(value==1/k)rc="1 "+k+"ème";
+    if(rc=="" && Math.trunc(value*100)==33)rc="un tiers d'1";
+    if(rc=="" && value>0.75)rc="presque 1";
+    if(rc=="" && value<0.5)rc="presque la moitié d'1"
+    if(value==0.5)rc="la moitié d'1";
+    if(rc=="" && value>0.5 && value<1)rc="plus de la moitié d'1"
+
+    // if(rc.length==0 && value<1) {
+    //   if (value * 10 == Math.trunc(value * 10))
+    //     rc = value * 100 + "% d'1";
+    // }
 
     if(rc.length>0)
       return(rc);
